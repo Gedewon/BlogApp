2543edad5a1395a2f19b39599bbbcb20bb26aaa1
gedewon <gedewon.hayle.9@gmail.com>
2022-08-19 20:27:58 +0300

    Add method to get 3 most recent posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments
   has_many :likes
+
+  def most_recent_post
+    Post.where(user:self).order(created_at: :desc).limit(3)
+  end
 end
