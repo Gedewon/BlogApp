331ef7b3bedab464ce604e7dc94650ac73cd2b3a
gedewon <gedewon.hayle.9@gmail.com>
2022-08-20 16:08:05 +0300

    Finshup Testing the hole method

--- spec/model/like_spec.rb
+++ spec/model/like_spec.rb
@@ -3,23 +3,23 @@ require 'rails_helper'
 RSpec.describe Like, type: :model do
   before :each do
     @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.', post_counter: 0)
+                              bio: 'Teacher from Mexico.', postscounter: 0)
     @first_user.save
 
-    @post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+    @post = Post.create(user_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
                         likes_counter: 0)
     @post.save
 
-    @like = Like.create(post_id: @post.id, author_id: @first_user.id)
+    @like = Like.create(post_id: @post.id, user_id: @first_user.id)
     @like.save
   end
   context 'Likes validations' do
     it 'validate update_likes_counter method' do
-      @like.update_likes_counter
+      @like.update_post_like_counter
       expect(@like.post.likes_counter).to be > 0
     end
     it 'Must belog to a user' do
-      @like.author_id = nil
+      @like.user_id = nil
       expect(@like).to_not be_valid
     end
 
--- spec/model/post_spec.rb
+++ spec/model/post_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   before :each do
     @first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-                              bio: 'Teacher from Mexico.', post_counter: 0)
+                              bio: 'Teacher from Mexico.', postscounter: 0)
     @first_user.save
 
-    @post = Post.create(author_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
+    @post = Post.create(user_id: @first_user.id, title: 'Hello', text: 'This is my first post', comments_counter: 0,
                         likes_counter: 0)
     @post.save
   end
@@ -32,16 +32,10 @@ RSpec.describe Post, type: :model do
     end
 
     it 'PostsCounter must be an integer greater than or equal to zero.' do
-      @post.update_posts_counter
+      @post.update_user_post_counter
       @post.save
-      expect(@post.author.post_counter).to be > 0
+      expect(@post.author.postscounter).to be > 0
     end
-    it 'validate recent comments method' do
-      comment1 = Comment.create(text: 'This is my first comment', post_id: @post.id, author_id: @first_user.id)
-      comment1.save
-      comment2 = Comment.create(text: 'This is my second comment', post_id: @post.id, author_id: @first_user.id)
-      comment2.save
-      expect(@post.recent_comments).to eq([comment1, comment2])
-    end
+
   end
 end
