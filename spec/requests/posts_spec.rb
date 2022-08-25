9195a405b9197ab8e3fda0a0ae0887af3b70bf3a
gedewon <gedewon.hayle.9@gmail.com>
2022-08-25 03:40:09 +0300

    Add controller test

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,16 +2,28 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
+    before(:all) do
+      get user_posts_path(user_id: 1)
+    end
     it 'returns http success' do
-      get '/posts/index'
+      puts response
       expect(response).to have_http_status(:success)
     end
   end
 
   describe 'GET /show' do
+    before(:each) do
+      get user_post_path(user_id: 1, id: 1)
+    end
     it 'returns http success' do
-      get '/posts/show'
       expect(response).to have_http_status(:success)
+    end
+    it 'returns http success' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'Render correct placeholder text' do
+      expect(response.body).to include('Here is a specific post for specific user')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,16 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
+    before(:each) do
+      get users_path
+    end
     it 'returns http success' do
-      get '/users/index'
       expect(response).to have_http_status(:success)
+    end
+    it 'Should Render the all user list' do
+      expect(response.body).to include('Here is a list of users')
+    end
+    it 'Render correct placeholder text' do
+      expect(response.body).to include('Here is a list of users')
     end
   end
 
   describe 'GET /show' do
-    it 'returns http success' do
-      get '/users/show'
+    before(:each) do
+      get user_path(id: 1)
+    end
+    it 'returns http success success' do
       expect(response).to have_http_status(:success)
+    end
+    it 'returns http success' do
+      expect(response).to render_template(:show)
+    end
+    it 'Render correct placeholder text' do
+      expect(response.body).to include('Here is a specific user')
     end
   end
 end
