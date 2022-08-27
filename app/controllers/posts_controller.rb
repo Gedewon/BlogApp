016721d605887b42865182b7ab8e4f8857548c25
gedewon <gedewon.hayle.9@gmail.com>
2022-08-27 15:26:19 +0300

    Create a Simple Form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,5 +6,30 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.includes(:comments, :user).find_by(user_id: params[:user_id], id: params[:id])
+    @user = current_user
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    parameters = post_params
+    post = Post.new(user_id: current_user.id, title: parameters[:title], text: parameters[:text], comments_counter: 0,
+                    likes_counter: 0)
+    post.save
+    # post.update_posts_counter
+    if post.save
+      # puts "Saved successfully"
+      redirect_to user_path(current_user)
+    else
+      # puts "Failed to save"
+      redirect_to new_user_post_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
