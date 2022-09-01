7ea0bee14d9067db565f8acc318d932c29265860
gedewon <gedewon.hayle.9@gmail.com>
2022-09-01 22:48:50 +0300

    Add devise for authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,18 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-    @posts = @user.most_recent_post
+    if params[:id] == 'sign_out'
+      sign_out current_user
+      redirect_to root_path
+    else
+      @user = User.find(params[:id])
+      @posts = @user.most_recent_post
+    end
   end
+
+
 end
