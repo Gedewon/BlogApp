04a70cbf54c84b83d0890af4d9e24ca30885a2a8
gedewon <gedewon.hayle.9@gmail.com>
2022-09-03 03:18:38 +0300

    Delete Comment  if authorized

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,13 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    flash[:notice] = "Comment ##{params[:id]} has been deleted"
+    redirect_back(fallback_location: root_path)
+  end
+
   def comment_params
     params.require(:comment).permit(:text)
   end
